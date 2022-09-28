e3fcce3fe406fb8ca362baefaf770fdcca8c211f
sourov <sourov.jsr98@gmail.com>
2022-09-29 00:10:53 +0600

    card added

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -1,16 +1,18 @@
 import React from 'react';
-
+import './Workout.css'
 const Workout = (props) => {
     console.log(props)
     const { picture, name, time, about, age } = props.exercise;
     return (
-        <div>
+        <div className='workout-container'>
             <img src={picture} alt=""></img>
-            <h4>{name}</h4>
-            <p>{about}</p>
-            <h6>For Age: {age}</h6>
-            <h6>Time Required: {time}</h6>
-
+            <div className='info'>
+                <h4>{name}</h4>
+                <p>{about}</p>
+                <p>For Age: {age}</p>
+                <p>Time Required: {time}s</p>
+            </div>
+            <button>Add to list</button>
         </div>
     );
 };
