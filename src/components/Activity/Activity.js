98243f14c61cab32a25ca5cc5e5e8de52b5a3fe7
sourov <sourov.jsr98@gmail.com>
2022-09-29 01:03:15 +0600

    Activity css added

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './Activity.css'
 const Activity = () => {
     return (
-        <div>
+        <div className='activity-container'>
             <div >
                 <h3>Mahmud Sourov</h3>
                 <p>Jessore, Bangladesh</p>
                 <div className='personal-info'>
-                    <div>
+                    <div className='weight'>
                         <h2>60<small>kg</small></h2>
                         <p>Weight</p>
                     </div>
-                    <div>
+                    <div className='height'>
                         <h2>5.7</h2>
                         <p>Height</p>
                     </div>
-                    <div>
-                        <h2>24<small>years</small></h2>
+                    <div className='age'>
+                        <h2>24<small>yrs</small></h2>
                         <p>Age</p>
                     </div>
                 </div>
@@ -53,6 +53,8 @@ const Activity = () => {
                     <p>Break Time:</p>
                 </div>
             </div>
+
+            <button className='activity-btn'>Activity Completed</button>
         </div>
     );
 };
