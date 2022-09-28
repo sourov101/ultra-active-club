07232401e84c8f6bc6ab859a0cca1f3344bc127e
sourov <sourov.jsr98@gmail.com>
2022-09-28 22:01:58 +0600

    Header component created

---
+++ src/components/Exrecise/Exercise.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Exercise.css'
+const Exercise = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Exercise;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Fight Club</h1>
+            <h3>“The first rule about fight club is you don't talk about fight club.”</h3>
+        </div>
+    );
+};
+
+export default Header;
