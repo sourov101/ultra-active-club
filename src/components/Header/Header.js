284ff9d47d96dda0d81470c08fcf087b3211a5f7
sourov <sourov.jsr98@gmail.com>
2022-09-29 16:17:42 +0600

    icon added anf final commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHand } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
     return (
-        <div className='header'>
-            <h1>Fight Club</h1>
-            <h3>“The first rule about fight club is you don't talk about fight club.”</h3>
+        <div>
+            <div className='header'>
+                <FontAwesomeIcon icon={faHand}></FontAwesomeIcon>
+                <h1>Fight Club</h1>
+
+            </div>
+            <div className='para'>
+                <h3>“The first rule about fight club is you don't talk about fight club.”</h3>
+            </div>
         </div>
     );
 };
